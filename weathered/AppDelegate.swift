5f09459413d533a29f1d59cfec2b9eddc3f5a29b
jmonmane <jmonmane@gmail.com>
2017-06-25 00:50:25 -0500

    Initial Commit

---
+++ weathered/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  weathered
+//
+//  Created by Beau Galbraith on 6/25/17.
+//  Copyright © 2017 Beau Galbraith. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ weathered/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  weathered
+//
+//  Created by Beau Galbraith on 6/25/17.
+//  Copyright © 2017 Beau Galbraith. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
